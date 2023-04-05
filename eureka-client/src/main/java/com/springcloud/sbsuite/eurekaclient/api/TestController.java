817f11c98cf787fcb158f66b19ef29639365345f
Alexei Ptitchkin <ptitchkin@gmail.com>
2023-04-05 14:54:43 -0400

    add custom routing

--- eureka-client/src/main/java/com/springcloud/sbsuite/eurekaclient/api/TestController.java
+++ eureka-client/src/main/java/com/springcloud/sbsuite/eurekaclient/api/TestController.java
@@ -2,12 +2,14 @@ package com.springcloud.sbsuite.eurekaclient.api;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/main")
 public class TestController {
     @GetMapping("/test")
     public String test() {
-        return "Test eureka-client 1 > Hello";
+        return "Test eureka-client 1 with custom reroute > Hello";
     }
 }
