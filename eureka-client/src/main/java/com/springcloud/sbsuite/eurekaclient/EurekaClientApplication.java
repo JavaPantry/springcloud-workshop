3d41cd0ab25f4c2afeb372b12540c57ed8724846
Alexei Ptitchkin <ptitchkin@gmail.com>
2023-04-05 11:22:31 -0400

    add eureka-client module

---
+++ eureka-client/src/main/java/com/springcloud/sbsuite/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,20 @@
+package com.springcloud.sbsuite.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ - adding `@EnableEurekaClient` **NOT RECOGNIZED**
+ - [EnableEurekaClient import doesn't exist](https://stackoverflow.com/questions/68285299/enableeurekaclient-import-doesnt-exist)
+    - @EnableEurekaClient is deprecated no need to annotate at springboot main applicationit is fine
+      if we add the `spring-cloud-starter-netflix-eureka-client` dependency in `pom`
+      and if we have the `spring.application.name=eclient` in yml or properties file it will be registered to Eureka Server
+ */
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+}
