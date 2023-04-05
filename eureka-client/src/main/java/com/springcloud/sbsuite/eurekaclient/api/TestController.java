65810e7c4b870825d9db00f575a81ab10a2d0cf9
Alexei Ptitchkin <ptitchkin@gmail.com>
2023-04-05 15:40:30 -0400

    test Load balancer in API Gateway

--- eureka-client/src/main/java/com/springcloud/sbsuite/eurekaclient/api/TestController.java
+++ eureka-client/src/main/java/com/springcloud/sbsuite/eurekaclient/api/TestController.java
@@ -1,6 +1,7 @@
 package com.springcloud.sbsuite.eurekaclient.api;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/main")
 public class TestController {
+
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
     @GetMapping("/test")
     public String test() {
-        return "Test eureka-client 1 with custom reroute > Hello";
+        // return text with instance id
+        return String.format("Test eureka-client (instance %s) > Hello", instanceId);
     }
 }
