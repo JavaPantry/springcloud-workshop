665709c726511e9dc6be94b4acaeb47912620af9
Alexei Ptitchkin <ptitchkin@gmail.com>
2023-05-31 16:41:24 -0400

    add validation annotations to ProductDto

--- inventory-service/src/main/java/com/springcloud/sbsuite/inventory/dto/ProductDto.java
+++ inventory-service/src/main/java/com/springcloud/sbsuite/inventory/dto/ProductDto.java
@@ -1,6 +1,8 @@
 package com.springcloud.sbsuite.inventory.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,6 +23,12 @@ public class ProductDto {
 
     @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate = null;
+
+    @NotBlank
+    @NotNull
     private String name;
+
+    @NotBlank
+    @NotNull
     private String description;
 }
