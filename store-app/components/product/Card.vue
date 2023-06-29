1e8894c6f59062eb8a72134c7aeb1c86f170baf5
Alexei Ptitchkin <ptitchkin@gmail.com>
2023-06-29 15:16:39 -0400

    Apply horizontal layout to ProductCard

--- store-app/components/product/Card.vue
+++ store-app/components/product/Card.vue
@@ -3,20 +3,30 @@ const { product } = defineProps(['product'])
 </script>
 
 <template>
-	<div class="card" style="width: 18rem;">
-		<img :src="`${product.image}`" class="card-img-top" :alt="`${product.title}`">
-		<div class="card-body">
-			<h3 class="card-title">{{ product.title }}</h3>
-			<h6 class="card-subtitle">Category: {{ product.category }}</h6>
-			<p class="card-text">{{ product.description }}</p>
-			<NuxtLink :to="`/products/${product.id}`">
-				<button class="btn btn-primary">View Details</button>
-			</NuxtLink>
+	<div class="card" style="width: 100%;">
+		<div class="row g-0">
+			<div class="col-md-2">
+				<img :src="`${product.image}`" class="img-fluid rounded-start" :alt="`${product.title}`">
+			</div>
+			<div class="col-md-10">
+				<div class="card-body">
+					<h3 class="card-title">{{ product.title }}</h3>
+					<h6 class="card-subtitle">Category: {{ product.category }}</h6>
+					<br>
+					<p class="card-text">{{ product.description }}</p>
+					<NuxtLink :to="`/products/${product.id}`">
+						<button class="btn btn-primary">View Details</button>
+					</NuxtLink>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
 
 
 <style scoped>
-
+img {
+	padding: 1rem;
+	height: 200px;
+}
 </style>
