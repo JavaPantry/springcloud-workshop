b2c7f51991b530d1f57d2951e0c0c0878d0292ea
Alexei Ptitchkin <ptitchkin@gmail.com>
2023-07-05 16:09:10 -0400

    remove commented plain html from store-app Details.vue

--- store-app/components/product/Details.vue
+++ store-app/components/product/Details.vue
@@ -3,24 +3,6 @@ const { product } = defineProps(['product'])
 </script>
 
 <template>
-	<!-- <div class="card" style="width: 100%;">
-		<h3 class="card-header">{{ product.title }}</h3>
-		<div class="row g-0">
-			<div class="col-md-2">
-				<img :src="`${product.image}`" class="img-fluid rounded-start" :alt="`${product.title}`">
-			</div>
-			<div class="col-md-10">
-				<div class="card-body">
-					<h6 class="card-subtitle">Category: {{ product.category }}</h6>
-					<br>
-					<p class="card-text">{{ product.description }}</p>
-					<NuxtLink :to="`/products/${product.id}`">
-						<button class="btn btn-primary">View Details</button>
-					</NuxtLink>
-				</div>
-			</div>
-		</div>
-	</div> -->
 	<v-card class="mx-auto">
 			<v-container fluid>
 		    	<v-row dense>
@@ -41,10 +23,3 @@ const { product } = defineProps(['product'])
 			</v-container>
 		</v-card>
 </template>
-
-<style scoped>
-
-img {
-	padding: 1rem;
-}
-</style>
